82352e3d9b10fe2e3af50fc3147469411d090c0c
Deevanshi <deevanshia56@gmail.com>
2018-03-31 00:36:33 +0530

    for check

---
+++ src/static/js/aadharv.js
@@ -0,0 +1,14 @@
+
+function validate()
+{
+    $('#age').onfocusout(function(){
+        alert();
+    });
+var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
+if(regexp1.test(document.getElementById("age").value))
+{
+alert("Only numbers are allowed");
+return false;
+}
+}
+
