4cd08d7e728e0c577a08532beb0fb991732cc426
Deevanshi <deevanshia56@gmail.com>
2018-03-31 04:03:39 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -2,14 +2,15 @@ $('#age').focusout(function(){
         var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
 if(regexp1.test($('#age').val()))
 {
+     $("#notfeedback").css('hide','block')
    $("#feedback").css('display','block')
-    $("#feedback").css('hide','block')
+
 
 return false;
 }else{
-
+ $("#feedback").css('hide','block')
     $("#notfeedback").css('display','block')
-    $("#notfeedback").css('hide','block')
+
 }
     });
 
