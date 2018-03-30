d2479662ce2474fe89be83674cb2f274560704e5
Deevanshi <deevanshia56@gmail.com>
2018-03-31 01:38:19 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -1,15 +1,14 @@
 $('#age').focusout(function(){
-    console.log('here');
-        alert();
-    });
-function validate()
-{
-
-var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
+        var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
 if(regexp1.test(document.getElementById("age").value))
 {
 alert("Only numbers are allowed");
 return false;
 }
+    });
+function validate()
+{
+
+
 }
 
