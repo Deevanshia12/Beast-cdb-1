55f85e81164b9d8f6885d8ac3167efafa13d250f
Deevanshi <deevanshia56@gmail.com>
2018-03-31 01:42:34 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -1,6 +1,6 @@
 $('#age').focusout(function(){
         var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
-if(regexp1.test(document.getElementById("age").value))
+if(regexp1.test($('#age').val()))
 {
 alert("sdfsdfdsf");
 return false;
