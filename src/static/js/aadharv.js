0c92a995fa6f51ffe610ff4bb6843f67e493c8ce
Deevanshi <deevanshia56@gmail.com>
2018-03-31 03:53:26 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -2,13 +2,12 @@ $('#age').focusout(function(){
         var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
 if(regexp1.test($('#age').val()))
 {
+   $("#feedback").css('display','block')
 
 return false;
 }else{
 
-    $("#feedback").css('display','block');
-     $("#age").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Invalid Aadhaar</div>");
-$("#age").parent().next(".validation").remove();
+    $("#notfeedback").css('display','block')
 }
     });
 
