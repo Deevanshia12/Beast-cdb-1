b4eec811de478b91f8a76d1b24bb309c3fe88712
Deevanshi <deevanshia56@gmail.com>
2018-03-31 12:20:38 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -1,26 +1,18 @@
 $('#age').focusout(function(){
-        var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
-        console.log(regexp1.test($('#age').val()));
+        var regexp1 = new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
 if(regexp1.test($('#age').val()))
 {
    $("#feedback").css('display','inline');
 
-
-
-}else{
+}
+else{
     $("#notfeedback").css('display','none');
 
 }
-    });
+    })
 
 
-function myFunction() {
-  var  myVar = setTimeout(Func, 300000);
-}
 
-function Func() {
-  location.reload();
-}
 
 
 
