01697b2ddce69bc7d5f13c5dfddc8432f7197efa
Deevanshi <deevanshia56@gmail.com>
2018-03-31 06:24:59 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -2,13 +2,11 @@ $('#age').focusout(function(){
         var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
 if(regexp1.test($('#age').val()))
 {
-     $("#notfeedback").css('display','none')
    $("#feedback").css('display','block')
 
 
 return false;
 }else{
- $("#feedback").css('display','none')
     $("#notfeedback").css('display','block')
 
 }
