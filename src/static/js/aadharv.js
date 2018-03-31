7001b2615f0030e8635e5bce6c18578f60e665c1
Deevanshi <deevanshia56@gmail.com>
2018-03-31 06:32:54 +0530

    for check

--- src/static/js/aadharv.js
+++ src/static/js/aadharv.js
@@ -1,16 +1,15 @@
-$('#age').focusout(function(){
-        var regexp1=new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
-if(regexp1.test($('#age').val()))
-{
-   $("#feedback").css('display','block')
+$('#age').focusout(function() {
+    var regexp1 = new RegExp("^(([\\d]{4}\\ ){2}[\\d]{4})|([\\d]{12})$");
+    if (regexp1.test($('#age').val())) {
+        $("#feedback").css('display', 'block')
 
 
-return false;
-}else{
-    $("#notfeedback").css('display','block')
+        return false;
+    } else {
+        $("#notfeedback").css('display', 'block')
 
-}
-    });
+    }
+  });
 
 
 
